020fad525e80a60fc4ab4458dc7c468cd3c9ebc5
imtaki <161586687+imtaki@users.noreply.github.com>
2025-09-25 21:52:27 +0200

    feat(signup): added signup page
    added signup page + (auth) directory

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function SignUpPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Sign Up Page</h1>
+        <p className="mt-4 text-lg text-muted-foreground">This is where the sign-up form will go. TBD</p>
+        </div>
+    );
+}
