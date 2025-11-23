4b396b590fa871954c884f08caf89d138928976d
Dominik Takáč <161586687+imtaki@users.noreply.github.com>
2025-11-23 23:47:07 +0100

    refactor: delete unused helpers

--- frontend/src/lib/getCookie.ts
+++
@@ -1,6 +0,0 @@
-
-export default function getCookie(name: string): string | null {
-  if (typeof document === "undefined") return null;
-  const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
-  return match ? decodeURIComponent(match[2]) : null;
-}
