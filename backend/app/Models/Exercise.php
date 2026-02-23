5eb2a643de33e6cc98237780cc96c314808096eb
Dominik Takáč <161586687+imtaki@users.noreply.github.com>
2026-02-23 22:19:25 +0100

    feat(relationship): added workout relationship to exercises

--- backend/app/Models/Exercise.php
+++ backend/app/Models/Exercise.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\WorkoutLog;
+
 class Exercise extends Model
 {
     use HasFactory;
@@ -25,5 +28,10 @@ class Exercise extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function workoutLogs()
+    {
+        return $this->hasMany(WorkoutLog::class);
     }
 }
